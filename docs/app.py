f0fa864782d3677b2d33affac4558b82d84757ef
Pavla Vesela <paja.v7@gmail.com>
2021-01-05 21:46:02 +0100

    Removing docs folder

--- docs/app.py
+++
@@ -1,7 +0,0 @@
-import dash
-
-# test: pridej nejake to CSS
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, title='Cross-Sell App', external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
-server = app.server
